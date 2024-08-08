1da42141355639009a765b710f8e0b37bc9b47ec
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-08 12:41:07 +0545

    config changed

--- src/models/client/config.ts
+++ src/models/client/config.ts
@@ -3,8 +3,8 @@ import env from "@/app/env";
 import { Client, Account, Avatars, Databases, Storage } from "appwrite";
 
 const client = new Client()
-    .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
-    .setProject(env.appwrite.projectId); // Your project ID
+    .setEndpoint(env.appwrite.endpoint) //  API Endpoint
+    .setProject(env.appwrite.projectId); //  project ID
 
 const databases = new Databases(client)
 const account = new Account(client);
